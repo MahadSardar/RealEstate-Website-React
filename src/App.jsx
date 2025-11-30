53efd9722246549143b9c13faf6bc7e0a69c545b
MahadSardar <mahadsardarm2@gmail.com>
2025-11-30 22:57:33 +0500

    Files upload

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import Header from './Components/Header'
+import About from './Components/About'
+import Projects from './Components/Projects'
+import Testimonails from './Components/Testimonails'
+import Contact from './Components/Contact'
+  import { ToastContainer, toast } from 'react-toastify';
+import Footer from './Components/Footer'
+
+const App = () => {
+  return (
+    <div className='w-full overflow-hidden'>
+      <ToastContainer/>
+      <Header/>
+      <About/>
+      <Projects/>
+      <Testimonails/>
+      <Contact/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
+
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react'
+import {assets} from '../assets/assets'
+
+const Navbar = () => {
+  const [showMenu,setShowMenu] = useState(false);
+
+  useEffect(()=>{
+    if(showMenu){
+      document.body.style.overflow = "hidden"
+    }
+    else{
+      document.body.style.overflow = "auto"
+    }
+    return ()=>{
+      document.body.style.overflow = "auto"
+    }
+  },[showMenu])
+  return (
+    <div className='absolute top-0 left-0 w-full z-10'>
+        <div className='container mx-auto flex justify-between items-center px-6 py-4 md:px-20 lg:px-32 bg-transparent'>
+            <img src={assets.logo} alt="" />
+            <ul className='hidden md:flex gap-7 text-white'>
+                <a href="#Header" className='cursor-pointer hover:text-gray-400'>Home</a>
+                <a href="#About" className='cursor-pointer hover:text-gray-400'>About</a>
+                <a href="#Projects" className='cursor-pointer hover:text-gray-400'>Projects</a>
+                <a href="#Testimonials" className='cursor-pointer hover:text-gray-400'>Testimonials</a>
+            </ul>
+            <button className='hidden md:block bg-white px-8 py-2 rounded-full'>Sign Up</button>
+            <img src={assets.menu_icon} alt="" className='md:hidden w-7 cursor-pointer' onClick={()=>{setShowMenu(true)}} />
+        </div>
+            {/*-------------Mobile-menu---------------  */}
+            <div className={`md:hidden ${showMenu ? "fixed w-full":"h-0 w-0"} top-0 right-0 bottom-0 overflow-hidden bg-white
+            transition-all`}>
+              <div className='flex justify-end p-6 cursor-pointer'>
+                <img src={assets.cross_icon} alt="" className='w-6' onClick={()=>{setShowMenu(false)}}/>
+              </div>
+              <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-bold'>
+                <a onClick={()=>{setShowMenu(false)}} href="#Header" className='px-4 py-2 rounded-full inline-block'>Home</a>
+                <a onClick={()=>{setShowMenu(false)}} href="#About" className='px-4 py-2 rounded-full inline-block'>About</a>
+                <a onClick={()=>{setShowMenu(false)}} href="#Projects" className='px-4 py-2 rounded-full inline-block'>Projects</a>
+                <a onClick={()=>{setShowMenu(false)}} href="#Testmonials" className='px-4 py-2 rounded-full inline-block'>Testmonials</a>
+              </ul>
+            </div>
+    </div>
+  )
+}
+
+export default Navbar
